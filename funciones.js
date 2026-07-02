aa3074aa5a0b8b27c5d588b7ed468327182d8f98
FabricioQs <iefc.kevin.quishpe@gmail.com>
2026-07-02 14:28:37 -0500

    Completada funcionalidad de calcular valor disponible mensual

--- funciones.js
+++ funciones.js
@@ -1,1 +1,10 @@
-//AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible (ingresos, egresos){
+    let resultado = ingresos-egresos;
+
+    if (resultado < 0) {
+        return 0; // Si es negativo, retorna 0
+    } else {
+        return resultado; // Si es positivo, retorna el valor real
+    }
+
+}
